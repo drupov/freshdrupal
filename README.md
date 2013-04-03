6ba43ea966f673fb315fdeb3a320e264a7bfea94
Dimitar Rupov <dimitar.rupov@dmcgroup.eu>
2013-04-03 09:53:15 +0200

    Remove omega text and fix some typos

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-You can create quickly a drupal environment for testing with this script. As it is only for local testing it assumes that the mysql user is root and there is no password. If that is not the case for your local dev environment you have to modify line 5 with the appropriate credentials for mysql.
+You can create quickly a drupal environment for testing with this script. As it is only for local testing it assumes that the mysql user is root and there is no password. If that is not the case for your local dev environment you have to modify line 5 and 15 with the appropriate credentials for mysql.
 
 Also feel free to add and enable modules that you think are important for your development or testing purposes. The modules that are available here are just the ones I find useful as a starting point.
 
-# Installation instructions
-type freshdrupal.sh project-name
+## Installation instructions
+type `freshdrupal.sh project-name`
--- freshdrupal.sh
+++ freshdrupal.sh
@@ -27,7 +27,7 @@ drush pm-enable ctools page_manager entity devel devel_generate views views_ui a
 # We need phpsass library for prepro and sassy. Only use this if you actually download and enable those two modules.
 git clone https://github.com/richthegeek/phpsass.git sites/all/libraries/phpsass
 
-# Download, create and enable a omega subtheme.
+# Download, create and enable an omega 3 subtheme.
 # drush dl omega omega_tools -y
 # drush en omega_tools -y
 # drush omega-subtheme $siteName --enable --set-default
@@ -36,44 +36,3 @@ git clone https://github.com/richthegeek/phpsass.git sites/all/libraries/phpsass
 # git init
 # git add .
 # git commit -m "Initial commit"
-
-# Print some info on configuring css and javascript, especially in omega.
-# echo ""
-# echo ""
-# echo ""
-# echo ""
-# echo "----- Use Omega 3 with Sassy -----"
-# echo "In order to use sass for your omega theme just go to your theme folder at sites/all/themes/$siteName"
-# echo "and open the file '$siteName.info'."
-# echo "Change '[global.css]' to '[global.scss]' in following lines:"
-# echo "css[global.css][name] = 'Your custom global styles'"
-# echo "css[global.css][description] = 'This file holds all the globally active custom CSS of your theme.'"
-# echo "css[global.css][options][weight] = '10'."
-# echo ""
-# echo "Clear the cache."
-# echo "Then in a browser go to your theme settings at 'admin/appearance/settings/$siteName'"
-# echo "and under 'Toggle styles' check the setting 'Your custom global styles (all) - global.scss'."
-# echo "As a last step go to your themes css folder at sites/all/themes/$siteName and change all"
-# echo "extensions of files with .css to .scss"
-# echo ""
-# echo ""
-# echo "----- Add custom js to Omega 3 -----"
-# echo "Add following lines to you .info file"
-# echo "libraries[custom_js][name] = Custom javascript"
-# echo "libraries[custom_js][description] = Custom javascript files for this theme."
-# echo "libraries[custom_js][js][0][file] = scripts.js"
-# echo "libraries[custom_js][js][0][options][weight] = 20"
-# echo "The 'scripts.js' file should reside in a js folder in your theme root directory."
-# echo ""
-# echo "Clear the cache."
-# echo "Then in a browser go to your theme settings at 'admin/appearance/settings/$siteName'"
-# echo "and under 'Toggle libraries' check the setting 'Custom javascript'."
-# echo ""
-# echo "In Drupal 7 your javascripts should be wrapped like this (e.g. in your 'scripts.js' file):"
-# echo "(function($) {"
-# echo "  Drupal.behaviors.myCustomBehavior = {"
-# echo "    attach: function(context) {"
-# echo "      alert('Success');"
-# echo "    }"
-# echo "  };"
-# echo "})(jQuery);"
